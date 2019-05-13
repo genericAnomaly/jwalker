bba73f7730b31b4472fc5d896992b563215640f9
GA <genericAnomaly@users.noreply.github.com>
2019-05-13 18:05:00 -0400

    Initial commit

---
+++ adventure.js
@@ -0,0 +1,70 @@
+var adventure = {
+    'meta'  :   {
+        /* Stuff defining the adventure will probably go here */
+        'name'      :   'Dennis Quest',
+        'author'    :   'Dennis',
+        'start'     :   'foyer'
+    },
+    'rooms'   :   {
+        'foyer' :   {
+            'img'   :   'foyer.png',
+            'map'   :   {
+                'stairs'    :   {
+                    'area'      :   {'shape' : 'poly', 'coords' : '220,0,390,110,485,105,400,0'},
+                    'hover'     :   {'cursor' : 'movement'},
+                    'click'     :   {'go' : 'upstairs'}
+                },
+                'door'      :   {
+                    'area'      :   {'shape' : 'rect', 'coords' : '577,187,695,434'},
+                    'hover'     :   {'cursor' : 'movement'},
+                    'click'     :   {'go' : 'outside'}
+                }
+            }
+        },
+        'upstairs'  :   {
+            'img'   :   'upstairs.png',
+            'map'   :   {
+                'stairs'  :   {
+                    'area'      :   {'shape' : 'rect', 'coords' : '510,440,800,600'},
+                    'hover'     :   {'cursor' : 'movement'},
+                    'click'     :   {'go' : 'foyer'}
+                }
+            }
+        },
+        'outside'  :   {
+            'img'   :   'outside.png',
+            'map'   :   {
+                'door'  :   {
+                    'area'      :   {'shape' : 'rect', 'coords' : '300,170,350,330'},
+                    'hover'     :   {'cursor' : 'movement'},
+                    'click'     :   {'go' : 'foyer'}
+                }
+            }
+        }
+    },
+    'variables'     : {
+        /*Reserved for further development, if the adventure needs to be able to store and check information about its state*/
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Hey Atom, suck my whitespace
---
+++ engine.js
@@ -0,0 +1,16 @@
+function goToRoom(id) {
+    var room = adventure.rooms[id];
+    var img = '<img src="img/'+room.img+'" usemap="#'+id+'_map" />';
+    var map = '<map name="'+id+'_map">';
+    for (var id in room.map){
+        map += '<area shape="'+room.map[id].area.shape+'" coords="'+room.map[id].area.coords+'" '+getHref(room.map[id].click)+' >';
+    }
+    map += '</map>';
+    document.getElementById('room').innerHTML = img + map;
+}
+
+function getHref(object) {
+    if ('go' in object) {
+        return 'href="javascript:goToRoom(\'' + object.go + '\');"';
+    }
+}
