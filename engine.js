ddadf6bd1ff773356ed3b0c21b88826bbc755591
GA <genericAnomaly@users.noreply.github.com>
2019-05-17 12:04:11 -0400

    cloned goToRoom to BuildRoom

--- engine.js
+++ engine.js
@@ -14,3 +14,19 @@ function getHref(object) {
         return 'href="javascript:goToRoom(\'' + object.go + '\');"';
     }
 }
+
+
+
+
+
+
+function buildRoom(id) {
+    var room = adventure.rooms[id];
+    var img = '<img src="img/'+room.img+'" usemap="#'+id+'_map" />';
+    var map = '<map name="'+id+'_map">';
+    for (var map_id in room.map){
+        map += '<area id="'+id+'_'+map_id+'" shape="'+room.map[map_id].area.shape+'" coords="'+room.map[map_id].area.coords+'" '+getHref(room.map[map_id].click)+' >';
+    }
+    map += '</map>';
+    document.getElementById('room').innerHTML = img + map;
+}
