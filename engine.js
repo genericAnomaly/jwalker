56cb8bbd2716b428eda3d597aa2f0efa693df835
GA <genericAnomaly@users.noreply.github.com>
2019-06-07 14:16:13 -0400

    Cleans up window.start and removes dead code

--- engine.js
+++ engine.js
@@ -5,19 +5,14 @@ function start() {
     //If the editor is requested, activate the editor
     if(window.location.hash == '#editor') startEditor();
 
-    //Activate interaction handler
+    //Invoke the Core Jinn
     InteractionJinn.invoke();
-
-    //Activate animation handler
     AnimationJinn.invoke();
 
-    //Register animationHandlers now that the AnimationJinn is invoked
-    //InteractionJinn.registerTextFadeAnimationHandler(); //I think yeah, all possible actions should have their own Jinn that registers with the core Jinn like Interaction and Animation after they've been invoked
-
+    //Invoke lesser Jinn
     TextJinn.invoke();
-
-    //Activate audio
     AudioJinn.invoke();
+    //TODO: write EuclidJinn for ``click.go`` property and to hold romm builder
 
 
 
@@ -98,7 +93,6 @@ class InteractionJinn {
         InteractionJinn.clickHandlers = {};
         InteractionJinn.register('sfx',   AudioJinn.playSFX);   //Should this be moved to the AudioJinn's invoke?
         InteractionJinn.register('go',    InteractionJinn.actionGo);
-        //InteractionJinn.register('text',  InteractionJinn.actionText);
     }
 
     static register(key, func) {
@@ -116,40 +110,6 @@ class InteractionJinn {
             }
         }
     }
-
-    //The InteractionJinn is currently responsible for the 'text' event, so it must also hold the code that makes text fade out.
-    //Should the 'text' event grow big enough to merit one, or the convention evolve such, it will be migrated outwards to its own dedicated Jinn
-
-    /*
-    static actionText(args) {
-        var svg = document.getElementById('overlay_svg');
-        var t = document.createElementNS(svg.namespaceURI, 'text');
-        svg.appendChild(t);
-
-        t = $(t)
-            .html(args.string)
-            .data('ttl', 5000)
-            .attr('x', '1em')
-            .attr('y', '1.5em');
-        //TODO: functionality to read in attributes like args.class
-    } */
-
-    /*
-    static registerTextFadeAnimationHandler() {
-        AnimationJinn.register(function(dt) {
-            $('#overlay_svg').find('text').each(function (index, value) {
-                var t = $(this);
-                var ttl = t.data('ttl') - dt;
-                t.data('ttl', ttl);
-                if (ttl < 1200) {
-                        t.css('opacity', ttl/1200);
-                }
-                if (ttl < 0) {
-                    t.remove();
-                }
-            });
-        })
-    }*/
 
     static actionGo(id) {
         AudioJinn.playTracks(adventure.rooms[id].tracks);
